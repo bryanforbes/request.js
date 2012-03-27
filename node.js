e0e3ac724be3f608468116ef7e34d3163b68c92d
Bryan Forbes <bryan@reigndropsfall.net>
2012-03-26 21:32:47 -0500

    Updates to node provider.

--- node.js
+++ node.js
@@ -7,24 +7,24 @@ define([
 	var http = require.nodeRequire('http'),
 		https = require.nodeRequire('https'),
 		URL = require.nodeRequire('url'),
-		undef;
+		undefined;
 
 	var defaultOptions = {
 		method: 'GET',
 		query: null,
-		data: undef,
+		data: undefined,
 		headers: {}
 	};
 	function request(url, options){
 		var args = util.parseArgs(url, util.deepCreate(defaultOptions, options));
 
-		var responseData = {
+		var response = {
 			url: url = args[0],
 			options: options = args[1]
 		};
 
 		var def = util.deferred(
-			responseData,
+			response,
 			function(dfd, data){
 				data.clientRequest.abort();
 				var err = data.error;
@@ -38,10 +38,10 @@ define([
 
 		url = URL.parse(url);
 
-		var reqOptions = responseData.requestOptions = {
+		var reqOptions = response.requestOptions = {
 			host: url.hostname,
 			method: options.method,
-			port: url.port == undef ? 80 : url.port,
+			port: url.port == null ? 80 : url.port,
 			headers: options.headers
 		};
 		if(url.path){
@@ -50,11 +50,11 @@ define([
 		if(options.user || options.password){
 			reqOptions.auth = (options.user||'') + ':' + (options.password||'');
 		}
-		var req = responseData.clientRequest = (url.protocol == 'https:' ? https : http).request(reqOptions);
+		var req = response.clientRequest = (url.protocol == 'https:' ? https : http).request(reqOptions);
 
 		req.on('response', function(response){
-			responseData.clientResponse = response;
-			responseData.status = response.statusCode;
+			response.clientResponse = response;
+			response.status = response.statusCode;
 
 			var body = [];
 			response.on('data', function(chunk){
@@ -65,9 +65,9 @@ define([
 				if(timeout){
 					clearTimeout(timeout);
 				}
-				responseData.text = body.join('');
-				handlers(responseData);
-				def.resolve(responseData);
+				response.text = body.join('');
+				handlers(response);
+				def.resolve(response);
 			});
 		});
 
@@ -75,16 +75,16 @@ define([
 			def.reject(e);
 		});
 
-		if(options.data !== undef){
+		if(options.data != null){
 			req.write(options.data);
 		}
 
 		req.end();
 
-		if(options.timeout != undef){
+		if(options.timeout != null){
 			var timeout = setTimeout(function(){
-				responseData.error = new Error('timeout exceeded');
-				responseData.error.dojoType = 'timeout';
+				response.error = new Error('timeout exceeded');
+				response.error.dojoType = 'timeout';
 				def.cancel();
 			}, options.timeout);
 		}
